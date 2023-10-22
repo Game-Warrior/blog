5242e14bc10f211a05e9d35146e8bff4170c8ce3
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-10-22 12:39:15 -0600

    updated nix flake

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
   };
 
   outputs = { self, nixpkgs }: {
-    packages."aarch64-darwin".defaultPackage = let
-      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
+    packages."aarch64-darwin".defaultPackage = let;
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin"
     in pkgs.buildEnv {
       name = "home-packages";
       paths = with pkgs; [
@@ -16,7 +16,6 @@
         jekyll
         ruby
         chruby
-        ruby-install
         xz
       ];
     };
