b2c1c0a982e9971e907af9bebc741993f05faec5
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-10-22 14:46:23 -0600

    added linux stuff for flake.nix

--- flake.nix
+++ flake.nix
@@ -36,5 +36,36 @@
         xz
       ];
     };
+    packages = {
+      x86_64-linux = {
+        defaultPackage = let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in pkgs.buildEnv {
+          name = "home-packages";
+          paths = with pkgs; [
+            # general tools
+            git
+            jekyll
+            ruby
+            chruby
+            xz
+          ];
+         };
+       };
+     };
+
+    devShells.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        # List of packages
+            git
+            jekyll
+            ruby
+            chruby
+            xz
+      ];
+    };
+  };
   };
 }
