1481bb7d7fac0d87fa72b2f942ed82c4ed36928d
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-10-22 13:00:36 -0600

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -6,12 +6,29 @@
   };
 
   outputs = { self, nixpkgs }: {
-    packages."aarch64-darwin".defaultPackage = let
-      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
-  in pkgs.buildEnv {
-      name = "home-packages";
-      paths = with pkgs; [
-        # general tools
+
+    packages.aarch64-darwin = {
+      defaultPackage = let
+        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+      in pkgs.buildEnv {
+          name = "home-packages";
+          paths = with pkgs; [
+            # general tools
+            git
+            jekyll
+            ruby
+            chruby
+            xz
+          ];
+        };
+     };
+
+    # Define a development shell for 'aarch64-darwin'
+    devShell.aarch64-darwin = let
+      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+    in pkgs.mkShell {
+      buildInputs = with pkgs; [
+        # List of packages for development
         git
         jekyll
         ruby
@@ -21,3 +38,4 @@
     };
   };
 }
+```
