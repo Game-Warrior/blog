6c9beee1baf15f1eab009155e5b9fca7f0bcd3c8
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-10-22 12:56:16 -0600

    updated nix flake

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
   outputs = { self, nixpkgs }: {
     packages."aarch64-darwin".defaultPackage = let
       pkgs = nixpkgs.legacyPackages."aarch64-darwin";
-    in pkgs.buildEnv {
+  in pkgs.buildEnv {
       name = "home-packages";
       paths = with pkgs; [
         # general tools
