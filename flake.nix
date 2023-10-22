d20cb892d8a2e10c6e1e2fb672ee7022aaafddc6
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-10-22 12:36:47 -0600

    updated flake.nix

--- flake.nix
+++ flake.nix
@@ -1,20 +1,16 @@
 {
   description = "The blog Flake";
-}
-# flake.nix skeleton:
 
-{
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    };
+  };
 
   outputs = { self, nixpkgs }: {
-    packages."aarch64-darwin".default = let
+    packages."aarch64-darwin".defaultPackage = let
       pkgs = nixpkgs.legacyPackages."aarch64-darwin";
     in pkgs.buildEnv {
       name = "home-packages";
       paths = with pkgs; [
-
         # general tools
         git
         jekyll
@@ -25,5 +21,4 @@
       ];
     };
   };
-
 }
