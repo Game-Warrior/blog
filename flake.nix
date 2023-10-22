c80c3f9a4c903a74a9023b3cf9fa97fd50fb4dc1
Game-Warrior <105872719+Game-Warrior@users.noreply.github.com>
2023-10-22 12:40:37 -0600

    added flake.nix and gemfile.lock

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
   };
 
   outputs = { self, nixpkgs }: {
-    packages."aarch64-darwin".defaultPackage = let;
-      pkgs = nixpkgs.legacyPackages."aarch64-darwin"
+    packages."aarch64-darwin".defaultPackage = let
+      pkgs = nixpkgs.legacyPackages."aarch64-darwin";
     in pkgs.buildEnv {
       name = "home-packages";
       paths = with pkgs; [
